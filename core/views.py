53c649ac837379f49d4f9562162e4018db437f78
vanessa <vanessa@vinta.com.br>
2018-07-27 17:33:34 -0300

    Formulários básicos com FBV

--- core/views.py
+++ core/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from core.helpers import get_character_name, ip_info
 from datetime import datetime
+from tuites.models import Tuite
 
 def index(request):
     context = {
-        'now': datetime.now()
+        'now': datetime.now(),
+        'tuites': Tuite.objects.all(),
     }
     return render(request, 'home.html', context)
--- tuites/views.py
+++ tuites/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from tuites.models import Tuite
 
-# Create your views here.
+def post_tuite(request):
+    context = {}
+
+    if request.method == 'POST':
+        print('Enviando formulário!')
+        # print(request.POST)
+        content = request.POST.get('content', None)
+        if content.isspace() or content == '':
+            context['error'] = 'Tuite não pode estar vazio!'
+        else:
+            Tuite.objects.create(
+                content=content,
+                author=request.user,
+            )
+            context['success_message'] = f'Seu Tuite de conteúdo "{content}" foi enviado'
+            
+    return render(request, 'post_tuite.html', context)
