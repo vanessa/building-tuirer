3b3a17c8f4ebe16fb009a6f33beedebbee41beee
vanessa <vanessa@vinta.com.br>
2018-07-30 21:38:49 -0300

    Preparação para a aula de amanhã

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    picture = models.ImageField('Foto de perfil', default='/img/blank-pic.png')
